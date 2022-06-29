0bb104bbef7ebc23ce03c9eda9f259af11b360cb
azeez1776 <aboud3am81@gmail.com>
2022-06-29 10:54:45 +0300

    :star: feat: added a contact api for the form

---
+++ pages/api/contact.ts
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+const  contact =  async (req:any, res:any) => {
+  
+    let nodemailer = require('nodemailer')
+
+    //Gmail service
+    const transporter = nodemailer.createTransport({
+      service:'Gmail',
+
+      //Email and password in the .env file
+      auth: {
+        user: process.env.email, 
+        pass: process.env.password  ,
+      },
+     
+    })
+
+    //the mail object
+    const mailData = { 
+      from: req.body.email,
+      to: process.env.email,
+      subject: `Message From ${req.body.name}`,
+      text: req.body.message + " | Sent from: " + req.body.email,
+      html: `<div>${req.body.message}</div><p>Sent from:
+      ${req.body.email}</p>`
+    }
+
+    //sending the mail
+    transporter.sendMail(mailData, function (err: any, info: any) {
+      if(err)
+        console.log(err)
+      else
+        console.log(info)
+    })
+    res.status(200)
+}
+
+export default contact
